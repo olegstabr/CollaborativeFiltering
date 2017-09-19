8e1a506c07de086b9586cd1c66ccced540033e4b
olegstabr <olegstabr@gmail.com>
2017-09-19 18:47:14 +0700

    Add calculate_error func

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import main
 
 
 # Расчет среднеквадратической ошибки
-def calculate_error():
-    pass
+def calculate_error(real_rating, calc_rating):
+    return pow(real_rating - calc_rating, 2) ** .5
 
 
 # Тестирование разработанной системы на тестовой выборке
-def test_data(path='./data/ml-100k/u.data'):
+def test_data(path='./data/ml-100k/u1.test'):
     # загружаем тестовые данные
     # вычисляем неизвестные оценки
     # вычисляем ошибку RMSE
