987ae2f437c41678dc81e03920cea74624539312
olegstabr <olegstabr@gmail.com>
2017-09-23 18:09:58 +0700

    TEST: Make test_data func

--- test.py
+++ test.py
@@ -12,4 +12,26 @@ def test_data(path='./data/ml-100k/u1.test'):
     # загружаем тестовые данные
     # вычисляем неизвестные оценки
     # вычисляем ошибку RMSE
-    pass
+    prefs = {}
+    with open(path) as file:
+        for line in file:
+            (user, movieId, rating, timestamp) = line.split("\t")
+            prefs.setdefault(user, {})
+            prefs[user][movieId] = float(rating)
+    user_id = str(1)
+    ratings = prefs[user_id]
+    for movie_id in ratings:
+        base_data = main.load_data()
+        real_rating = prefs[user_id][movie_id]
+        calc_rating = main.get_rating(base_data, user_id, int(movie_id))
+        if calc_rating == 0:
+            continue
+        print('Фильм = %s\nРеальная оценка = %f\nПосчитанная оценка = %f\nОшибка = %f\n' % (
+            movie_id, real_rating, calc_rating, calculate_error(real_rating, calc_rating)))
+
+
+def test():
+    test_data()
+
+
+test()
