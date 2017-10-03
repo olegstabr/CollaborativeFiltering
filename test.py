7011a7552f2ceb58336f269543373b2635816164
olegstabr <olegstabr@gmail.com>
2017-10-03 18:50:01 +0700

    TEST: Add test for item-based type

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import main
+import user_based
+import item_based
 
 
 # Расчет среднеквадратической ошибки
@@ -12,13 +13,13 @@ def calculate_error(real_rating, calc_rating):
 
 
 # Тестирование разработанной системы на тестовой выборке
-def test_data(path='./data/ml-100k/u1.test'):
+def test_data(path='./data/ml-100k/u1.test', load_data=user_based.load_data):
     # загружаем тестовые данные
     # вычисляем неизвестные оценки
     # вычисляем ошибку RMSE
     test_data = {}
-    base_data = main.load_data()
-    user_id = str(1)
+    base_data = load_data()
+    user_id = str(1) # Будет искаться оценка для юзера 1 или для фильма 1
     real_ratings = []
     calc_ratings = []
     with open(path) as file:
@@ -28,21 +29,28 @@ def test_data(path='./data/ml-100k/u1.test'):
             test_data[user][movieId] = float(rating)
     ratings = test_data[user_id]
     for movie_id in ratings:
-        calc_rating = main.get_rating(base_data, user_id, int(movie_id))
+        calc_rating = user_based.get_rating(base_data, user_id, int(movie_id), similarity=user_based.sim_distance_2)
         if calc_rating == 0:
             continue
-        calc_ratings.append(calc_rating)
+        calc_ratings.append(round(calc_rating, 1))
         real_rating = test_data[user_id][movie_id]
         real_ratings.append(real_rating)
+    if load_data == user_based.load_data:
+        filter_type = "user_based:"
+    else:
+        filter_type = "item_based:"
+    print filter_type
     print("Реальные оценки:      %s" % real_ratings)
     print("Предсказанные оценки: %s" % calc_ratings)
     print("Ошибка: %s" % calculate_error(real_ratings, calc_ratings))
+    print
         # print('Фильм = %s\nРеальная оценка = %f\nПосчитанная оценка = %f\nОшибка = %f\n' % (
         #     movie_id, real_rating, calc_rating, calculate_error(real_rating, calc_rating)))
 
 
 def test():
     test_data()
+    test_data(load_data=item_based.load_data)
 
 
 test()
